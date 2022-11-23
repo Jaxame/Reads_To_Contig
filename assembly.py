2cdc2168fc004083cb3b2ec256545955f8fb51f8
JACKY AME <jacky.ame@orange.fr>
2022-11-23 16:24:24 +0100

    change variables

--- assembly.py
+++ assembly.py
@@ -6,8 +6,8 @@ from read import *
 from contig import *
 
 with open("dna_reads.txt", "r") as file:
-    lines = [line.strip().upper() for line in file]
-    lreads = [Read(elt) for elt in lines]
+    reads = [line.strip().upper() for line in file]
+    lreads = [Read(read) for read in reads]
     contig1 = Contig(lreads[0].get_seq())
     print(lreads[0])
     print(contig1)
